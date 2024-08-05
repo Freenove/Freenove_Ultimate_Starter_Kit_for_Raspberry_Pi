7c1fed458b46e12e27dac2d390e3037207c9cf7d
Zhentao-Lin <173039613@qq.com>
2024-08-05 10:06:33 +0800

    Update build.sh

--- Libs/C-Libs/ADCDevice/build.sh
+++ Libs/C-Libs/ADCDevice/build.sh
@@ -3,7 +3,7 @@
 current_file=$0
 cd "$(dirname "$current_file")"
 
-g++ -fPIC -c ADCDevice.cpp
+g++ -fPIC -c ADCDevice.cpp -o ADCDevice.o
 g++ -shared -fPIC -o libADCDevice.so ADCDevice.o
 sudo cp ADCDevice.hpp /usr/include/
 sudo cp libADCDevice.so /usr/lib
