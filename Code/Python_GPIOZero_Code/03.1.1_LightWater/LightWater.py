7f947e47f08ffe93e362cd55b8e417501a2bfdd2
Zhentao-Lin <173039613@qq.com>
2024-08-14 16:32:26 +0800

    Update LightWater.py

--- Code/Python_GPIOZero_Code/03.1.1_LightWater/LightWater.py
+++ Code/Python_GPIOZero_Code/03.1.1_LightWater/LightWater.py
@@ -5,24 +5,22 @@
 # Author      : www.freenove.com
 # modification: 2023/05/11
 ########################################################################
-from gpiozero import LEDBoard
+from gpiozero import LED
 from time import sleep
 
-#ledPins = ["J8:11", "J8:12","J8:13","J8:15","J8:16","J8:18","J8:22","J8:3","J8:5","J8:24"]
 ledPins = [17, 18, 27, 22, 23, 24, 25, 2, 3, 8]
-
-leds = LEDBoard(*ledPins, active_high=False)
-
+leds = [LED(pin=pin) for pin in ledPins] 
+    
 def loop():
     while True:
         for index in range(0,len(ledPins),1):      # make led(on) move from left to right
-            leds.on(index)  
+            leds[index].on()  
             sleep(0.1)
-            leds.off(index)
+            leds[index].off() 
         for index in range(len(ledPins)-1,-1,-1):   #move led(on) from right to left
-            leds.on(index)      
+            leds[index].on()  
             sleep(0.1)
-            leds.off(index)
+            leds[index].off() 
 
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
@@ -30,4 +28,7 @@ if __name__ == '__main__':     # Program entrance
         loop()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         print("Ending program")
+    finally:
+        for index in range(0,len(ledPins),1): 
+            leds[index].close()  
 
